30325a1e4ba20cbe91ec8969e86e1342497a16bc
mh4125 <hargrove_michaela@students.columbusstate.edu>
2023-10-04 22:13:35 +0000

    cypress setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
+    e2e:{
+        setupNodeEvents(on, config){
             //implement node event listeners here
         },
     },
